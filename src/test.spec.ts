65f65084d41f26df63708630b0ff3dfdcec93c1c
Thiago Leite <leite.tjs@gmail.com>
2024-03-01 15:27:09 -0300

    initial commit

---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+
+test("sum 2 numbers", () => {
+	expect(1 + 3).toEqual(4);
+});
---
+++ tsup.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src", "!src/__tests__/**", "!src/**/*.test.*"],
+});
